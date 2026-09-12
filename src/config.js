1f3df5903e4ca357a9d29abccdc64216d9a70db4
meeeeeooooowwwwwww <generalflynn17@gmail.com>
2026-09-12 14:46:55 +1200

    chore(home): update featured Rumble video

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const SITE = {
   fullName: "Natalie G. Winters",
   domain: "https://nataliegwinters.com",
   substackHome: "https://nataliegwinters.substack.com/",
-  rumbleEmbed: "https://rumble.com/embed/v7d6zhq/?pub=4kxtac",
+  rumbleEmbed: "https://rumble.com/embed/v7d83by/?pub=4kxtac",
 
   images: {
     portrait:
